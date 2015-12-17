dd96a6cf14cf2dbf983867200862d64c535bfdcc
Marc Schmitt <marc.schmitt.mls@gmail.com>
2015-12-17 15:03:57 -0500

    Initial commit.

---
+++ app/src/main/java/edu/rosehulman/schmitml/namebaseadpater/NameAdapter.java
@@ -0,0 +1,76 @@
+package edu.rosehulman.schmitml.namebaseadpater;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by schmitml on 12/17/15.
+ */
+public class NameAdapter extends BaseAdapter {
+    private Context mContext;
+    final ArrayList<String> mNames = new ArrayList<>();
+    private Random mRandom = new Random();
+
+    public NameAdapter(Context context){
+        mContext = context;
+        for(int i = 0; i < 5 ; i++){
+            mNames.add(getRandomName());
+        }
+    }
+
+    private String getRandomName() {
+        String[] names = new String[]{
+                "Hannah", "Emily", "Sarah", "Madison", "Brianna",
+                "Kaylee", "Kaitlyn", "Hailey", "Alexis", "Elizabeth",
+                "Michael", "Jacob", "Matthew", "Nicholas", "Christopher",
+                "Joseph", "Zachary", "Joshua", "Andrew", "William"
+        };
+        return names[mRandom.nextInt(names.length)];
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return mNames.size();
+    }
+
+    // Don't Care
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    // Don't Care
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = null;
+        //Recycle an existing view
+        if(convertView == null){
+            view = LayoutInflater.from(mContext).inflate(R.layout.name_view, parent, false);
+        }else{
+            view = convertView;
+        }
+
+        TextView nameTextView = (TextView) view.findViewById(R.id.name_view);
+        TextView positionTextView = (TextView) view.findViewById(R.id.position_view);
+        String name = mNames.get(position);
+
+        nameTextView.setText(name);
+        positionTextView.setText("I'm #" + position);
+
+        return view;
+    }
+}
